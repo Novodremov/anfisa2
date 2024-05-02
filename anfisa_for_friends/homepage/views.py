3c8f8c045482f28718755275ba05508cc52f1c6c
Alexey Lysov <novice83@mail.ru>
2024-05-02 14:09:19 +0300

    Пробный коммит для проекта с репозитория Яндекса

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+        ).filter(
+            Q(is_published=True)
+            & (Q(is_on_main=True) | Q(title__contains='пломбир')))
+    # Полученный из БД QuerySet передаём в словарь контекста:
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
+    return render(request, template, context)
